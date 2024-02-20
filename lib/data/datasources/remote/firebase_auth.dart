ebeadfdcdaf7d65b30089e6092d0e17e1c5e37a7
dhiasaibi <dhiasaibi@yahoo.com>
2024-02-20 15:33:14 +0100

    storing document in firestore after login

--- lib/data/datasources/remote/firebase_auth.dart
+++ lib/data/datasources/remote/firebase_auth.dart
@@ -1,24 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  // instance of firestore
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   User? get currentUser => _firebaseAuth.currentUser;
 
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    // Get the current user after signing in
+    User? user = _firebaseAuth.currentUser;
 
-Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+    if (user != null) {
+      // Create a new document for the user in the users collection
+      await _fireStore.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'email': user.email,
+      });
+    }
+  }
 
-Future<void> signInWithEmailAndPassword({
-  required String email,
-  required String password,
-}) async {
-  await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-}
-
-Future<void> signOut() async {
-  await _firebaseAuth.signOut();
-}
-
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
 }
--- lib/data/datasources/remote/user_remote_data_source.dart
+++ lib/data/datasources/remote/user_remote_data_source.dart
@@ -1,47 +1,34 @@
-
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:glumate_flutter/core/config.dart';
 import 'package:glumate_flutter/core/errors/failure.dart';
 import 'package:glumate_flutter/data/models/user_model.dart';
 
 abstract class UserRemoteDataSource {
-  Future<void> patientRegister({required PatientRequest body });
-
-
+  Future<void> patientRegister({required PatientRequest body});
 }
-
 
 class UserRemoteDataSourceImpl implements UserRemoteDataSource {
-final Dio dio ;
+  final Dio dio;
 
-UserRemoteDataSourceImpl({required this.dio});
-
+  UserRemoteDataSourceImpl({required this.dio});
 
   @override
-Future<void> patientRegister({required PatientRequest body}) async {
-const patientRegisterURL = "${AppConfig.baseUrl}user/PatientRegister" ;
-try {
-await dio.post ( patientRegisterURL , data: body.toJson());
-
-} on DioException catch (e){
-
-
-  if (e.response != null && e.response!.statusCode == 400) {
-      print('Email already exists: ${e.response!.statusCode}');
-      throw EmailExistsFailure();
-    } else {
-      print('Error registering user: ${e.message}');
-      throw ServerFailure(); 
-    }
-}
-
-
-catch (e){
-  
+  Future<void> patientRegister({required PatientRequest body}) async {
+    const patientRegisterURL = "${AppConfig.baseUrl}user/PatientRegister";
+    try {
+      await dio.post(patientRegisterURL, data: body.toJson());
+    } on DioException catch (e) {
+      if (e.response != null && e.response!.statusCode == 400) {
+        print('Email already exists: ${e.response!.statusCode}');
+        throw EmailExistsFailure();
+      } else {
+        print('Error registering user: ${e.message}');
+        throw ServerFailure();
+      }
+    } catch (e) {
       throw AppFailure();
-}
-
+    }
   }
-  
 }
